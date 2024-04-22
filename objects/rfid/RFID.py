5c74f9831affff6e16980474b730729d94558382
IlanOu <ilan.outyey>
2024-04-22 07:57:52 +0200

    Adding RFID system

---
+++ objects/rfid/RFID.py
@@ -0,0 +1,56 @@
+
+# Using simple MFRC522
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+
+# Créer une instance du module RFID
+MIFAREReader = SimpleMFRC522()
+
+# Bienvenue
+print("Lecture de cartes RFID...")
+
+# Cette boucle détecte les cartes RFID
+try:
+    while True:
+        id, text = MIFAREReader.read()
+        
+        print("id : ")
+        print(id)
+        print("text : ")
+        print(text)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+
+"""
+
+# Using MFRC522
+
+import RPi.GPIO as GPIO
+from mfrc522 import MFRC522
+
+# Créer une instance du module RFID
+MIFAREReader = MFRC522(bus=0, device=0)
+
+# Bienvenue
+print("Lecture de cartes RFID...")
+
+# Cette boucle détecte les cartes RFID
+try:
+    while True:
+        # Attendre qu'une carte soit détectée
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # Si une carte est détectée
+        if status == MIFAREReader.MI_OK:
+            print("Carte détectée")
+
+        # Faire quelque chose quand une carte est détectée...
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+"""
---
+++ objects/rfid/readme.md
@@ -0,0 +1,17 @@
+# How to install rfid libs
+
+```bash
+git clone https://github.com/pimylifeup/MFRC522-python.git
+```
+
+Then, you should have a folder added named `MFRC522-python`
+
+```bash
+cd MFRC522-python
+```
+
+Once you're in the folder, you can install the library :
+
+```bash
+sudo python setup.py install
+```
