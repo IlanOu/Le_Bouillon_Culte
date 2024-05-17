2b2c7ad3894af2e907cfdd4637ca1808cf2836f5
anthony74742 <antho230104@gmail.com>
2024-05-17 14:38:30 +0200

    Add websocket

--- src/toolbox/Websocket.py
+++ src/toolbox/Websocket.py
@@ -1,42 +1,33 @@
-from src.objects.joystick.StateJoystick import *
+import threading
+from websocket_server import WebsocketServer
+import time
 
-import asyncio
-import websockets
-import json
-
-class WebSocketServer:
+# Définition de la classe du serveur WebSocket
+class WebSocketServerThread(threading.Thread):
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.server = None
+        threading.Thread.__init__(self)
+        self.server = WebsocketServer(host, port)
+        self.server.set_fn_new_client(self.new_client)
+        self.server.set_fn_client_left(self.client_left)
+        self.server.set_fn_message_received(self.message_received)
 
-    async def connection_handler(self, websocket, path):
-        try:
-            async for message in websocket:
-                jsonReceive = json.loads(message)
-                match jsonReceive["action"]:
-                    case "joystick":
-                        if jsonReceive["data"] == "tire":
-                            Joystick.set_current_state(StateJoystick.DOWN)
-                            print("joystick tirer")
-                        else:
-                            Joystick.set_current_state(StateJoystick.UP)
-                            print("joystick lacher")
-                            
-                    case _:
-                        print("default")
-        finally:
-            pass
+    def run(self):
+        self.server.run_forever()
 
-    def start(self):
-        start_server = websockets.serve(self.connection_handler, self.host, self.port)
-        asyncio.get_event_loop().run_until_complete(start_server)
-        print(f"Serveur WebSocket démarré sur {self.host}:{self.port}")
-        asyncio.get_event_loop().run_forever()
-    
-    def check_action(self):
-        pass
-""" 
+    def new_client(self, client, server):
+        print(f"Nouveau client connecté : {client['id']}")
+
+    def client_left(self, client, server):
+        print(f"Client déconnecté : {client['id']}")
+
+    def message_received(self, client, server, message):
+        print(f"Message reçu de {client['id']} : {message}")
+        server.send_message_to_all(f"Client {client['id']} a dit : {message}")
+
+# Création et démarrage du serveur WebSocket
 if __name__ == "__main__":
-    server = WebSocketServer("0.0.0.0", 8080)
-    server.start() """
+    host = "0.0.0.0"
+    port = 8080
+    server_thread = WebSocketServerThread(host, port)
+    print(f"Serveur WebSocket démarré sur {host}:{port}")
+    server_thread.start()
