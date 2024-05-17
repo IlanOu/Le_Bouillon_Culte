c3597ca2e72746fe8726aabe0e33464381ace19b
Nay <ilan.outyey@gmail.com>
2024-05-17 12:00:54 +0200

    Display error on screen

--- src/objects/Checker.py
+++ src/objects/Checker.py
@@ -1,14 +1,15 @@
 from src.objects.SensorsChecker import SensorsChecker
 from src.toolbox.Debug import Debug
 
+from src.objects.displayer.WebDisplayer import WebApp
 
 class Checker:
     def __init__(self) -> None:
         self.sensors_checker = SensorsChecker()
-        
 
     def check_sensors(self):
         datas = self.sensors_checker.check_sensors()
         if not datas["pass"]:
+            WebApp().show(datas["message"],"stop")
             Debug.LogError(datas["message"])
             return
