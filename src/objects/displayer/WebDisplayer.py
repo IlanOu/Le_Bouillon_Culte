5c5484db0cc5379578397e2e6400767e3e8931e4
Nay <ilan.outyey@gmail.com>
2024-05-27 10:06:48 +0200

    Images and json fix

--- src/objects/displayer/WebDisplayer.py
+++ src/objects/displayer/WebDisplayer.py
@@ -126,7 +126,10 @@ class WebApp(object):
                 content = image_url
             elif mode == "3images":
                 for image in content.split("|"):
+                    image = image.strip()
+                    Debug.LogFatSeparator("\"" + image + "\"")
                     image_url = url_for('static', filename=f"images/{image}", _external=True)
+                    # image_url.replace("%20", "")
                     image_urls.append(image_url)
 
                 content = "|".join(image_urls)
