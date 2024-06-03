200c51f052c2d3ad7ead029860aafaf3df7ef383
IlanOu <ilan.outyey@gmail.com>
2024-06-03 15:58:21 +0200

    reset displayer

--- src/objects/displayer/WebDisplayer.py
+++ src/objects/displayer/WebDisplayer.py
@@ -1,51 +1,35 @@
 from flask import Flask, render_template, Response, url_for, request, jsonify
 import threading
 import logging
-
 from src.toolbox.Debug import *
-
 from src.Config import Config
-
 import socket
-
 """
 Lancer le WebDisplayer :
-
 webApp = WebApp(update_interval=1) # initialiser le displayer
 webApp.show("Le texte que l'on veut") # Pour afficher le texte que l'on veut
-
 webApp.show("Le texte que /n l'on veut") # Pour afficher le texte que l'on veut avec un retour à la ligne [ Attention c'est un /n, pas un \n ! ]
-
 Afficher un tableau :
-
 webApp.show("A | B | C | D") # Tableau simple
 webApp.show("Titre ~ A | B | C | D") # Tableau avec un titre
 """
 
-
-
-
 class StringUpdater:
     def __init__(self, update_interval=5):
-        self.content = ""
+        self.text = "..."
         self.update_interval = update_interval
         self.update_event = threading.Event()
-       
-    def show(self, content):
-        self.content = str(content)
+
+    def show(self, text: str):
+        self.text = str(text)
         self.update_event.set()
-        Debug.LogWhisper("[Display]> " + str(content))
-
-
-
-
+        Debug.LogWhisper("[Display]> " + text)
 
 class WebApp(object):
     _instance = None
-
+  
     def __new__(cls, update_interval=5):
         if cls._instance is None:
-
             cls._instance = super(WebApp, cls).__new__(cls)
             
             ip_adress = ""
@@ -67,19 +51,15 @@ class WebApp(object):
             
             cls._instance.page_loaded = False
             
-
             # Removing useless logs
             log = logging.getLogger('werkzeug')
             log.setLevel(logging.ERROR)
-
             
             # Home page
             # ---------------------------------------------------------------------------- #
             @cls._instance.app.route('/')
             def home():
                 return render_template('index.html', string=cls._instance.string_updater.text)
-
-
             # To refresh page 
             # ---------------------------------------------------------------------------- #
             @cls._instance.app.route('/stream')
@@ -89,10 +69,7 @@ class WebApp(object):
                         yield f"data: {cls._instance.string_updater.text}\n\n"
                         cls._instance.string_updater.update_event.wait()
                         cls._instance.string_updater.update_event.clear()
-
                 return Response(generate_stream(), mimetype='text/event-stream')
-
-
             # Check if page has been loaded for client
             # ---------------------------------------------------------------------------- #
             @cls._instance.app.route('/page-loaded', methods=['POST'])
@@ -106,13 +83,11 @@ class WebApp(object):
                 else:
                     return jsonify({'message': 'Erreur lors de la vérification du chargement de la page.'})
             
-
         return cls._instance
     
     def exit(self):
         exit(0)
         
-
     def get_ip_address(self):
         """
         Retourne l'adresse IP de l'appareil.
@@ -127,29 +102,28 @@ class WebApp(object):
             Debug.LogError(f"Erreur lors de la récupération de l'adresse IP : {e}")
             return None
 
-    def show(self, content):
-        # with self.app.app_context():
-        #     image_urls = []
+    def show(self, content, mode="text"):
+        with self.app.app_context():
+            image_urls = []
 
-        #     if mode == "image":
-        #         image_url = url_for('static', filename="images/" + content, _external=True)
-        #         content = image_url
-        #     elif mode == "3images":
-        #         for image in content.split("|"):
-        #             image = image.strip()
-        #             Debug.LogFatSeparator("\"" + image + "\"")
-        #             image_url = url_for('static', filename=f"images/{image}", _external=True)
-        #             # image_url.replace("%20", "")
-        #             image_urls.append(image_url)
+            if mode == "image":
+                image_url = url_for('static', filename="images/" + content, _external=True)
+                content = image_url
+            elif mode == "3images":
+                for image in content.split("|"):
+                    image = image.strip()
+                    Debug.LogFatSeparator("\"" + image + "\"")
+                    image_url = url_for('static', filename=f"images/{image}", _external=True)
+                    # image_url.replace("%20", "")
+                    image_urls.append(image_url)
 
-        #         content = "|".join(image_urls)
+                content = "|".join(image_urls)
 
         if not self.is_running:
-            
-            self.string_updater.show(content)
+            self.string_updater.show(mode + "||" + content)
 
             self.server = threading.Thread(target=self.app.run, kwargs={'debug': False, 'use_reloader': False})
             self.server.start()
             self.is_running = True
         else:
-            self.string_updater.show(content)
+            self.string_updater.show(mode + "||" + content)
