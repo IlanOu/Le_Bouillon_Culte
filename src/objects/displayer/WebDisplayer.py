781f9ec2ea405515432a0d813bce855136eaa3f2
IlanOu <ilan.outyey@gmail.com>
2024-06-03 15:14:53 +0200

    converting and send datas success

--- src/objects/displayer/WebDisplayer.py
+++ src/objects/displayer/WebDisplayer.py
@@ -1,96 +1,93 @@
-from flask import Flask, render_template, Response, url_for, request, jsonify
+from flask import Flask, render_template, Response, request, jsonify
 import threading
 import logging
-
-from src.toolbox.Debug import *
-from src.toolbox.Singleton import singleton
-
+from src.toolbox.Debug import Debug
 from src.Config import Config
-
 import socket
-
+import json
 
 class StringUpdater:
     def __init__(self):
-        self.content = []
+        self.text = ""
         self.update_event = threading.Event()
        
-    def show(self, content):
-        self.content = content
+    def show(self, text):
+        self.text = text
         self.update_event.set()
-        Debug.LogWhisper("[Display]> " + str(jsonify(content)))
+        Debug.LogWhisper("[Display]> " + str(text))
 
-
-
-@singleton
 class WebApp(object):
-    def __init__(self):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(WebApp, cls).__new__(cls)
             
-        ip_adress = ""
-        try:
-            ip_adress = WebApp().get_ip_address()
-        except:
-            ip_adress = Config().hotspot_ip
-        
-        Debug.LogSeparator(f"Server is running on : {ip_adress}:5000")
-        
-        WebApp().app = Flask(__name__)
-        WebApp().app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-        WebApp().app.config['SERVER_NAME'] = f'{ip_adress}:5000'
-        WebApp().app.config['APPLICATION_ROOT'] = '/'
-        WebApp().app.config['PREFERRED_URL_SCHEME'] = 'http'
-        WebApp().string_updater = StringUpdater()
-        WebApp().is_running = False
-        WebApp().server = None
-        
-        WebApp().page_loaded = False
-        
-
-        # Removing useless logs
-        log = logging.getLogger('werkzeug')
-        log.setLevel(logging.ERROR)
-
-        
-        # Home page
-        # ---------------------------------------------------------------------------- #
-        @WebApp().app.route('/')
-        def home():
-            return render_template('index.html')
-
-
-        # To refresh page 
-        # ---------------------------------------------------------------------------- #
-        @WebApp().app.route('/stream')
-        def stream():
-            def generate_stream():
-                while True:
-                    yield f"data: {jsonify(WebApp().string_updater.content).get_json()}\n\n"  # Envoyer le contenu JSONifié
-                    WebApp().string_updater.update_event.wait()
-                    WebApp().string_updater.update_event.clear()
-
-            return Response(generate_stream(), mimetype='text/event-stream')
-
-
-        # Check if page has been loaded for client
-        # ---------------------------------------------------------------------------- #
-        @WebApp().app.route('/page-loaded', methods=['POST'])
-        def page_loaded_event():
-            data = request.get_json()
-            if data and data.get('loaded'):
-                # Enregistrer l'information de chargement de page pour l'utilisateur
-                WebApp().page_loaded = True
-                Debug.LogWhisper("[Log]> Écran cliqué")
-                return jsonify({'message': 'La page a été chargée côté client.'})
-            else:
-                return jsonify({'message': 'Erreur lors de la vérification du chargement de la page.'})
+            try:
+                ip_address = cls._instance.get_ip_address()
+            except:
+                ip_address = Config().hotspot_ip
+            
+            Debug.LogSeparator(f"Server is running on : {ip_address}:5000")
+            
+            cls._instance.app = Flask(__name__)
+            cls._instance.app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+            cls._instance.app.config['SERVER_NAME'] = f'{ip_address}:5000'
+            cls._instance.app.config['APPLICATION_ROOT'] = '/'
+            cls._instance.app.config['PREFERRED_URL_SCHEME'] = 'http'
+            cls._instance.string_updater = StringUpdater()
+            cls._instance.is_running = False
+            cls._instance.server = None
+            
+            cls._instance.page_loaded = False
             
 
+            # Removing useless logs
+            log = logging.getLogger('werkzeug')
+            log.setLevel(logging.ERROR)
+
+            
+            # Home page
+            @cls._instance.app.route('/')
+            def home():
+                Debug.prefixActive = False
+                with cls._instance.app.app_context():
+                    return render_template('index.html')
+
+
+            # To refresh page 
+            @cls._instance.app.route('/stream')
+            def stream():
+                def generate_stream():
+                    while True:
+                        with cls._instance.app.app_context():
+                            yield f"data: {json.dumps(cls._instance.string_updater.text, ensure_ascii=False)}\n\n"
+                            cls._instance.string_updater.update_event.wait()
+                            cls._instance.string_updater.update_event.clear()
+
+                return Response(generate_stream(), mimetype='text/event-stream')
+
+
+
+
+            # Check if page has been loaded for client
+            # ---------------------------------------------------------------------------- #
+            @cls._instance.app.route('/page-loaded', methods=['POST'])
+            def page_loaded_event():
+                data = request.get_json()
+                if data and data.get('loaded'):
+                    cls._instance.page_loaded = True
+                    Debug.LogWhisper("[Log]> Écran cliqué")
+                    return jsonify({'message': 'La page a été chargée côté client.'})
+                else:
+                    return jsonify({'message': 'Erreur lors de la vérification du chargement de la page.'})
+            
+        return cls._instance
     
     def exit(self):
         exit(0)
         
 
-   
     def get_ip_address(self):
         """
         Retourne l'adresse IP de l'appareil.
@@ -105,12 +102,14 @@ class WebApp(object):
             Debug.LogError(f"Erreur lors de la récupération de l'adresse IP : {e}")
             return None
 
-    def show(self, content):
-        if not self.is_running:
-            self.string_updater.show(content)
 
-            self.server = threading.Thread(target=self.app.run, kwargs={'debug': False, 'use_reloader': False})
-            self.server.start()
-            self.is_running = True
-        else:
-            self.string_updater.show(content)
+    def show(self, content):
+        with self.app.app_context():
+            if not self.is_running:
+                self.string_updater.show(content)
+
+                self.server = threading.Thread(target=self.app.run, kwargs={'debug': False, 'use_reloader': False})
+                self.server.start()
+                self.is_running = True
+            else:
+                self.string_updater.show(content)
--- test.py
+++ test.py
@@ -2,17 +2,16 @@ from src.objects.displayer.WebDisplayer import WebApp
 
 
 object = [
-            {
-                "type" : "text",
-                "content": "je suis un grand titre",
-                "style" : ["grosTitre", "Rouge"]
-            },
-            {
-                "type": "image",
-                "content": "url/de/limage.png",
-                "style": ["centré", "grosse image"]
-            }
-        ]
+    {
+        "type" : "text",
+        "content": "je suis un grand titre",
+        "style" : ["grosTitre", "Rouge"]
+    },
+    {
+        "type": "image",
+        "content": "url/de/limage.png",
+        "style": ["centré", "grosse image"]
+    }]
 
 
 WebApp().show(object)
